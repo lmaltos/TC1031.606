846a3779d9b5b71f90654ce7207af9b83f93c3d2
lmaltos <136147644+lmaltos@users.noreply.github.com>
2023-10-02 14:20:46 +0000

    Accede a lista enlazada por posicion

--- Bloque_2/LinkedList.h
+++ Bloque_2/LinkedList.h
@@ -8,6 +8,7 @@ class LinkedList {
     private:
     node<T> *head;
     int size;
+    node<T>* getByPos(int);
   public:
     LinkedList() {
         head = NULL;
@@ -15,12 +16,30 @@ class LinkedList {
     }
     ~LinkedList();
     void addFist(T);
+    void addLast(T);
+    T operator[](int);
     void print();
 };
 
 template <class T>
-LinkedList<T>::~LinkedList() {
+void deleteRecursivo(node<T>* nodo) {
+    if (nodo->getNext() != NULL)
+        deleteRecursivo(nodo->getNext());
+    std::cout << nodo << "\t" << nodo->getData() << std::endl;
+    delete nodo;
+}
 
+template <class T>
+LinkedList<T>::~LinkedList() {
+    node<T> *nodo,*next;
+    //deleteRecursivo(head);
+    nodo = head;
+    while (nodo != NULL) {
+        next = nodo->getNext();
+        std::cout << nodo << "\t" << nodo->getData() << std::endl;
+        delete nodo;
+        nodo = next;
+    }
 }
 
 template <class T>
@@ -28,6 +47,42 @@ void LinkedList<T>::addFist(T data) {
     node<T>* nuevoNodo;
     nuevoNodo = new node<T>(data,head);
     head = nuevoNodo;
+    size++;
+}
+
+template <class T>
+void LinkedList<T>::addLast(T data) {
+    node<T>* nuevoNodo, *last;
+    nuevoNodo = new node<T>(data,NULL);
+    if (head != NULL) {
+        last = head;
+        while (last->getNext() != NULL) {
+            last = last->getNext();
+        }
+        last->setNext(nuevoNodo);
+    }
+    else {
+        head = nuevoNodo;
+    }
+    size++;
+}
+
+template <class T>
+node<T>* LinkedList<T>::getByPos(int k) {
+    node<T> *nodo;
+    if (k < 0 || k > size)
+        throw std::out_of_range("k pos out of range");
+    nodo = head;
+    for (int i = 0;i < k;i++) {
+        nodo = nodo->getNext();
+    }
+    return nodo;
+}
+
+template <class T>
+T LinkedList<T>::operator[](int k) {
+    node<T> *nodo = getByPos(k);
+    return nodo->getData();
 }
 
 template <class T>
--- Bloque_2/main.cpp
+++ Bloque_2/main.cpp
@@ -1,4 +1,6 @@
+#include <iostream>
 #include "LinkedList.h"
+using namespace std;
 
 int main() {
     LinkedList<int> lista;
@@ -8,5 +10,16 @@ int main() {
     lista.addFist(4);
     lista.addFist(5);
     lista.addFist(6);
+    lista.print();
+    lista.addLast(1);
+    lista.addLast(2);
+    lista.addLast(3);
+    lista.addLast(4);
+    lista.addLast(5);
+    //cout << "lista.getByPos(4) = " << lista.getByPos(4) << endl;
+    cout << "lista[4] = " << lista[4] << endl;
+    cout << "lista[5] = " << lista[5] << endl;
+    cout << "lista[6] = " << lista[6] << endl;
+    cout << "lista[7] = " << lista[7] << endl;
     lista.print();
 }
